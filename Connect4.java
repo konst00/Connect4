731a7566f0a64c84ea26e829f326aa69d24f6efe
wiesenthal <2006184@student.acalanes.k12.ca.us>
2017-03-15 11:02:42 -0700

    initial

---
+++ Connect4.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connect4;
+
+/**
+ *
+ * @author 2006184
+ */
+public class Connect4 {
+
+    private int[][] board = new int[6][7];
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    //getBoard
+    
+    //askMove1
+    
+    //askMove2
+    
+    //checkValid
+    
+    //checkWin
+    
+}
